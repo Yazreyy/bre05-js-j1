6ac2b75f9d2b99b0814a2e2112cd51cc6919d6a5
Marlonn Gillet <gillet.marlonn56@icloud.com>
2026-01-06 09:21:15 +0000

    execution js

--- exercices-variables/index.js
+++ exercices-variables/index.js
@@ -1,3 +1,20 @@
-const character = "Legolas";
-console.log(character);
-character = "Gimli";
+let name = "Legolas";
+console.log(name);
+name = "Gimli";
+console.log(name);
+
+
+let number = 358;
+console.log(number);
+
+let deci = 2.88552;
+console.log(deci);
+
+let nega = -5;
+console.log(nega);
+
+let oui = true;
+console.log(oui);
+
+let non = false;
+console.log(non);
